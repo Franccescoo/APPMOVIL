3b6159e78bf4cd735d8457e79ec615074ff658e2
MatiasPenalozaJara <matiaspenalozajarapj6@gmail.com>
2022-08-28 18:50:06 -0400

    ayuda cliente, y titulos

---
+++ src/app/pages/ayuda-cliente/ayuda-cliente.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ayuda-cliente',
+  templateUrl: './ayuda-cliente.page.html',
+  styleUrls: ['./ayuda-cliente.page.scss'],
+})
+export class AyudaClientePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
