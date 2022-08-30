6dc54516c34243d98303e6b73a328d6287ad9abf
ZHEKO <fr.artuso@duocuc.cl>
2022-08-30 18:53:58 -0400

    cambios necesarios, PD: funciona mas o menos ubicacion

--- src/app/pages/finalizar-viaje/finalizar-viaje.page.ts
+++ src/app/pages/finalizar-viaje/finalizar-viaje.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-finalizar-viaje',
@@ -9,8 +9,22 @@ import { AlertController } from '@ionic/angular';
 export class FinalizarViajePage implements OnInit {
 
   constructor(private alertController: AlertController) {}
+  @ViewChild(IonModal) modal: IonModal;
   ngOnInit() {
   }
+
+  message = '';
+  name: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'Cancelar');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+
 
   async presentAlert() {
     const alert = await this.alertController.create({
