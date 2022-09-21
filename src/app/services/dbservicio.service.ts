6fb5239451efb14f97a979fe9ac0b51674564313
ZHEKO <fr.artuso@duocuc.cl>
2022-09-21 17:20:25 -0300

    servicios de base de datos

---
+++ src/app/services/dbservicio.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@awesome-cordova-plugins/sqlite/ngx';
+import { AlertController, Platform } from '@ionic/angular';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbservicioService {
+
+  constructor() { }
+}
