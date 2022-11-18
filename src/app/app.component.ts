293563cfd266c9479a52f153c04ff5cc41ffac38
MatiasPenalozaJara <matiaspenalozajarapj6@gmail.com>
2022-11-17 21:53:48 -0300

    MENU PRUEBA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { DbservicioService } from './services/dbservicio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  idrol: any=[];
+  idrol='';
+  Usuario: any[] = []
 
-  constructor(public nativeStorage: NativeStorage) {}
+  constructor(public nativeStorage: NativeStorage,private bd: DbservicioService,private router: Router) {}
+
+  ngOnInit() {
+    this.bd.dbState().subscribe((res) => {
+      if (res) {
+        this.bd.fetchUser().subscribe(item => {
+          this.Usuario = item;
+
+        })
+      }
+    })
+  }
 
   guardaridrol(){
     this.nativeStorage.getItem('idrol').then((data4)=>{
       this.idrol = data4
     })
+  }
+  inicio(){
+    if (this.idrol == "1") {
+      this.router.navigate(['/inicio-conductor']);
+    } else {
+      if (this.idrol == "2") {
+        this.router.navigate(['/inicio-cliente']);
+      }
+
+    }
+  }
+  mapa(){
+    if (this.idrol == "1") {
+      this.router.navigate(['/mapa-conductor']);
+    } else {
+      if (this.idrol == "2") {
+        this.router.navigate(['/mapa-cliente']);
+      }
+
+    }
+  }
+  perfil(){
+    if (this.Usuario[0].id_rol == 1) {
+      this.router.navigate(['/perfil-conductor']);
+    } else {
+      if (this.Usuario[0].id_rol == 2) {
+        this.router.navigate(['/pefil-cliente']);
+      }
+
+    }
   }
 
 }
