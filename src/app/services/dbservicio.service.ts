f0cb88262950315e98175df8d9b29264325c626a
Franccesco <101402711+Franccescoo@users.noreply.github.com>
2022-11-20 19:27:12 -0300

    kj

--- src/app/services/dbservicio.service.ts
+++ src/app/services/dbservicio.service.ts
@@ -237,11 +237,6 @@ export class DbservicioService {
     });
   }
 
-
-
-
-
-  
   agregarAuto(patente, modelo, marca, puestos, fk_idusuario) {
     let data = [patente, modelo, marca, puestos, fk_idusuario];
     return this.database.executeSql('INSERT INTO auto (  patente , modelo , marca , puestos , fk_idusuario) VALUES (? , ? , ? , ? , ?)', data).then(res => {
